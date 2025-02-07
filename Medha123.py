940c03f14f2f2a094da17aab63d44e52a505043d
MedhaBK <medhabk05@gmail.com>
2025-02-07 10:56:41 +0530

    Add files via upload

---
+++ Medha123.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.title("Welcome Everyone we are learning Python <3 :)")
+st.write("Pyhton requires practice")
+data = pd.DataFrame({'c1': [23, 45, 67, 89],'c2': ['A','B','C','D']})
+
+st.write("Below is the table for data: ")
+st.write(data)
+
+chart_data = pd.DataFrame(np.random.randn(20,3), columns= ['A','B','C'])
+st.line_chart(chart_data)
+st.bar_chart(chart_data)
+st.area_chart(chart_data)
